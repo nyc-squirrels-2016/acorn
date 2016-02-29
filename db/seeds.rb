efec44d5f1991302a8a3b47297301f9757ad8b43
Dan Andersen <dandersen2@gmail.com>
2016-02-29 10:47:43 -0500

    Add squirrel model and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Squirrel.create!(name: 'Sniffy', color: 'red', breed:'red', weight: '1.1', date_of_birth: '2015-11-22' )
+Squirrel.create!(name: 'Fluffy', color: 'gray', breed:'gray', weight: 1.83848, date_of_birth: '2015-07-04' )
